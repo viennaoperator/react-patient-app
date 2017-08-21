63f60b3cd70e959d026e0baa4f92676c23840a9b
harald.florjan <harald.florjan@ebcont.com>
2017-08-21 20:48:45 +0200

    bugfix

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../App.css';
 import { Button, Form} from 'react-bootstrap';
 
-class App extends Component {
+export class App extends Component {
   render() {
     return (
         <div className="App">

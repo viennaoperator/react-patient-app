a585a6ac0b8f9805b1897f97ef101cb7fd02fa37
harald.florjan <harald.florjan@ebcont.com>
2017-08-21 20:42:33 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Start the app
+```
+npm install
+npm start
+```
+
+# Assignment Description
+## Point 1 - Setup
+Hot LESS Reloading solved via less-watch-compiler. Needed dependencies and npm config:
+```
+"dependencies": {
+    "less": "^2.7.2",
+    "less-watch-compiler": "^1.7.0",
+    "npm-run-all": "^4.0.2",
+  }
+```
+
+```
+"scripts": {
+    "build-css": "lessc ./src/less/app.less ./src/App.css",
+    "watch-css": "less-watch-compiler src/less src App.less",
+    "start-js": "react-scripts start",
+    "start": "npm-run-all -p watch-css start-js",
+    "build": "npm run build-css && react-scripts build"
+}
+```
+
+## Point 2 - CRUD Patient
+
+
+## Point 4 - Server Side Rendering
+I used this tutorial to enable server side rendering
+https://medium.com/disdj/server-side-rendering-and-create-react-app-aka-react-scripts-f68411da7438
---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import './App.css';
+import { Button, Form} from 'react-bootstrap';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+            <Button bsStyle="success">Add Patient</Button>
+            <h2>PatientSky Patients:</h2>
+            <Form>
+                <Input placeholder="search for patient.."/>
+            </Form>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Birthday</th>
+                        <th>Phone Number</th>
+                        <th>Controls</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Harald</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>
+                            <Button bsStyle="primary">Edit Patient</Button>
+                            <Button bsStyle="danger">Delete Patient</Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+  }
+}
+
+const Input = (props) => {
+    return (
+        <input placeholder={props.placeholder}></input>
+    )
+}
+
+export default App;
